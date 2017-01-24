acd099599cfce3cf0e220db6af4472c4c6108680
doinavirlan <doinavirlan72@gmail.com>
2017-01-24 09:38:11 -0800

    Add files via upload

---
+++ lab4/src/button.h
@@ -0,0 +1,14 @@
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+#include <avr/io.h>
+
+void initButtonOne();
+void initButtonTwo();
+int isButtonOnePressed();
+int isButtonTwoPressed();
+
+
+
+
+#endif /* BUTTON_H_ */
